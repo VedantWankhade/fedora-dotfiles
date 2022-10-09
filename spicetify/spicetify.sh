509c5fa162c00bbc2168168e89a10d52df29c1a7
VedantWankhade <vedantwankhade17@gmail.com>
2022-10-09 21:47:30 +0530

    spicetify setup

---
+++ spicetify/spicetify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing spicetify"
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
+
+echo "Changing spotify-client permissions"
+sudo chmod a+wr /usr/share/spotify-client
+sudo chmod a+wr /usr/share/spotify-client/Apps -R
+
+echo "Editing spicetify config.nini"
+sed 's/spotify =/spotify = \/usr\/share\/spotify-client/' ~/.config/spicetify/config-xpui.ini
+
+echo "Installing spicetify marketplace"
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
