aa487b53f843948f20ae142cd5aba34666f3ff3b
VedantWankhade <vedantwankhade17@gmail.com>
2022-10-09 22:07:50 +0530

    backup before installing markteplace

--- spicetify/spicetify.sh
+++ spicetify/spicetify.sh
@@ -10,5 +10,7 @@ sudo chmod a+wr /usr/share/spotify-client/Apps -R
 echo "Editing spicetify config.nini"
 sed 's/spotify =/spotify = \/usr\/share\/spotify-client/' ~/.config/spicetify/config-xpui.ini
 
+spicetify backup apply
+
 echo "Installing spicetify marketplace"
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
